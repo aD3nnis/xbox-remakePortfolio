e3e7098d73e8cb6fd6c6a4839382142cb56a597b
aD3nnis <avadennis117@gmail.com>
2024-01-29 11:49:13 -0800

    comit1

---
+++ entries/main/pages/index.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+.home.page
+    //- Title based on window size. If SSR, this will prevent cumulative layout shift.
+    client-only
+      h1 {{ title }}
+      h1(slot="placeholder") Loading...
+    p Lucas ipsum dolor sit amet coruscant anakin jar obi-wan darth darth organa hoth antilles chewbacca. Sebulba han obi-wan antilles obi-wan. C-3po zabrak padmé gonk ackbar yavin solo. Darth alderaan palpatine dagobah grievous dooku twi'lek maul moff. Mara tusken raider kamino skywalker hutt mothma wedge greedo vader. Yavin kamino ben kenobi sidious lando. Kenobi calrissian organa ventress k-3po yoda darth jinn. Darth binks boba ben wedge. Lando twi'lek mace ewok solo. Skywalker kenobi coruscant kessel droid.
+    p Solo c-3p0 watto hutt chewbacca r2-d2 windu. Grievous obi-wan jade organa calamari jinn aayla. Windu yavin boba amidala darth secura mara boba ben. Solo greedo hutt fett padmé antilles kamino. Mara zabrak obi-wan binks gamorrean mon. Binks twi'lek dooku moff. Jango leia binks calrissian luuke wedge wampa. Darth sidious binks biggs. Yoda lars antilles darth calamari skywalker antilles dagobah. Qui-gonn darth kessel fisto kessel organa darth. Organa maul vader darth moff ben obi-wan. Yoda kamino fett darth.
+    .button-wrapper
+      button(@click="causeError") Cause Error
+      nuxt-link.btn(to="/sample") Sample Page
+      ButtonCounter
+</template>
+
+<script>
+import ButtonCounter from '~~/components/ButtonCount.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ButtonCounter,
+  },
+  computed: {
+    title() {
+      if (this.$store.state.isMobile) {
+        return 'Home - Mobile'
+      } else if (this.$store.state.isTablet) {
+        return 'Home - Tablet'
+      } else {
+        return 'Home - Desktop'
+      }
+    },
+  },
+  methods: {
+    causeError() {
+      throw new Error('ERROR')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+// .home {
+// }
+.button-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 2rem;
+
+  button,
+  .btn {
+    margin: 0.5rem 0;
+  }
+
+  @include media-breakpoint-up(md) {
+    flex-direction: row;
+
+    button,
+    .btn {
+      margin: 0 0.5rem;
+    }
+  }
+}
+</style>
---
+++ entries/main/pages/vimeoStuff.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+.video-container_box
+  .box_video
+    client-only
+      vue-vimeo-player.testing( ref="player" :video-id="videoID"  :options="options" :player-height="height" :player-width="width")
+    //-  <div class="main-tool"></div>    //  v-bind
+    //-    <div class="scrollable"></div>
+</template>
+<script>
+import { vueVimeoPlayer } from 'vue-vimeo-player'
+
+export default {
+  components: { vueVimeoPlayer },
+  data () {
+    return {
+      width: '500px',
+      videoID: '783455878',
+      options: {
+        muted: true,
+        autoplay: true
+      }
+    }
+  }
+}
+</script>
+<style>
+.video-container_box {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: aquamarine;
+  position: relative;
+}
+.box_video{
+  position: relative;/* 16:9 aspect ratio (change this value to match your video's aspect ratio) */
+  height: 80%;
+  width: 80%;
+  background-color: bisque;
+}
+.box_video .testing {
+  /* position: absolute; */
+  /* display: flex; */
+  width: 100%;
+  height: 100%;
+}
+.box_video iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
